a3feefa8b2f1e24aac293bfee522fefe2be00d5f
Kd3Us <jules.sorrentino@gmail.com>
2025-07-02 10:39:46 +0200

    Substeps projects

--- starterKit-front/src/environments/environment.development.ts
+++ starterKit-front/src/environments/environment.development.ts
@@ -1,13 +1,13 @@
 export const environment = {
     production: false,
-    apiProjectURL: 'https://localhost:3009/projects',
+    apiProjectURL: 'http://localhost:3009/projects',
     //apiWorkspaceURL: 'http://18.169.1.118:3009/workspaces',
-    apiWorkspaceURL: 'https://localhost:3009/workspaces',
+    apiWorkspaceURL: 'http://localhost:3009/workspaces',
     apiProjectManagementURL: 'https://localhost:3009/project-management',
-    apiTeamsURL: 'https://localhost:3009/teams',
+    apiTeamsURL: 'http://localhost:3009/teams',
   //  externaleAuthUrl: "https://startupkit.distribute.app/",
-    externaleAuthUrl: "https://startupkit.speedpresta.com/",
+    externaleAuthUrl: "http://startupkit.speedpresta.com/",
    // verifyTokenUrl: "https://api.speedpresta.com/api/v1/verify/token",
-   verifyTokenUrl: "https://localhost:3009/auth/login",
+   verifyTokenUrl: "http://localhost:3009/auth/login",
     jwtSecret: "votre_secret_jwt"
 };
